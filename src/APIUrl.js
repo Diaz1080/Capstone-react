b0339443eed7f30f990970e62a1ac1bb7d9b6014
Diaz1080 <Nydia1080@yahoo.com>
2023-03-12 18:53:23 -0400

    Update APIUrl.js

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,2 +1,2 @@
-export default "http://localhost:3001";
-// export default "https://api.syracuse-food-pantry-easy-search.org";
+// export default "http://localhost:3001";
+export default "https://api.syracuse-food-pantry-easy-search.org";
