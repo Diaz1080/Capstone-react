1294ec1f23b1ba9daf1f606af549338dbf937e8f
Diaz1080 <Nydia1080@yahoo.com>
2023-03-12 18:22:12 -0400

    Create NewPantryDashboard.js

---
+++ src/contact/NewPantryDashboard.js
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import APIUrl from "../APIUrl";
+
+const NewPantryDashboard = () => {
+  const [updates, setUpdates] = useState([]);
+  const updateNewTable = async () => {
+    const updateResponse = await fetch(`${APIUrl}/New`);
+    const updateData = await updateResponse.json();
+    setUpdates(updateData.Newupdates);
+  };
+
+  useEffect(() => {
+    updateNewTable();
+  }, []);
+
+  const approve = async (id) => {
+    const NewResponse = await fetch(`${APIUrl}/approveUpdate/${id}`);
+    const data = await NewResponse.json();
+    if (data.error) {
+      alert(data.error);
+    } else {
+      alert("Pantry Created");
+      updateNewTable();
+    }
+  };
+
+  return (
+    <div>
+      <h1>Admin</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Company Name</th>
+            <th scope="col">Company Name Continued</th>
+            <th scope="col">Adress</th>
+            <th scope="col">City State Zip</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Day1</th>
+            <th scope="col">Time1</th>
+            <th scope="col">Day2</th>
+            <th scope="col">Time2</th>
+            <th scope="col">Day3</th>
+            <th scope="col">Time3</th>
+            <th scope="col">Day4</th>
+            <th scope="col">Time4</th>
+            <th scope="col">Day5</th>
+            <th scope="col">Time5</th>
+            <th scope="col">Facebook</th>
+            <th scope="col">Instagram</th>
+            <th scope="col">Twitter</th>
+            <th scope="col">LinkedIn</th>
+            <th scope="col">Text1</th>
+            <th scope="col">Text2</th>
+            <th scope="col">Text3</th>
+            <th scope="col">Text4</th>
+            <th scope="col">Text5</th>
+            <th scope="col">Text6</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {updates.map((update) => {
+            return (
+              <tr key={update.id}>
+                <th scope="row">{update.companyName}</th>
+                <th scope="row">{update.companyNameContinued}</th>
+                <th scope="row">{update.Adress}</th>
+                <th scope="row">{update.cityStateZip}</th>
+                <th scope="row">{update.phone}</th>
+                <td>{update.day1}</td>
+                <td>{update.time1}</td>
+                <td>{update.day2}</td>
+                <td>{update.time2}</td>
+                <td>{update.day3}</td>
+                <td>{update.time3}</td>
+                <td>{update.day4}</td>
+                <td>{update.time4}</td>
+                <td>{update.day5}</td>
+                <td>{update.time5}</td>
+                <td>{update.facebook}</td>
+                <td>{update.intagram}</td>
+                <td>{update.twitter}</td>
+                <td>{update.linkeIn}</td>
+                <td>{update.text1}</td>
+                <td>{update.text2}</td>
+                <td>{update.text3}</td>
+                <td>{update.text4}</td>
+                <td>{update.text5}</td>
+                <td>{update.text6}</td>
+                <td>
+                  <span
+                    style={{ color: "blue", textDecoration: "underline" }}
+                    onClick={() => {
+                      approve(update.id);
+                    }}
+                  >
+                    Approve
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default NewPantryDashboard;
