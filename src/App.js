9f29206801485978cbb07cccb7ba0c8c488952cb
Diaz1080 <Nydia1080@yahoo.com>
2023-03-19 20:06:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import New from "./contact/New";
 import Contact from "./contact/Contact";
 import Signup from "./contact/Signup";
 import PantryUpdate from "./contact/PantryUpdate";
-import FoodbankUpdate from "./contact/foodbankUpdate";
+// import FoodbankUpdate from "./contact/foodbankUpdate";
 import Direction from "./pages/Direction";
 import AdminDashboard from "./contact/AdminDashboard";
 import NewPantryDashboard from "./contact/NewPantryDashboard";
@@ -23,7 +23,7 @@ function App() {
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/PantryUpdate" element={<PantryUpdate />} />
-          <Route path="/FoodbankUpdate" element={<FoodbankUpdate />} />
+          {/* <Route path="/FoodbankUpdate" element={<FoodbankUpdate />} /> */}
           <Route path="/AdminDashboard" element={<AdminDashboard />} />
           <Route path="/NewPantryDashboard" element={<NewPantryDashboard />} />
         </Routes>
